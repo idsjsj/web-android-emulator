da349c54824b4f4b3b69e5869a4795b777dc8103
idsjsj <idsjsj12@gmail.com>
2025-10-19 11:03:35 +0900

    Update comments and formatting in start-emulator.sh

--- start-emulator.sh
+++ start-emulator.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH
 
@@ -9,12 +8,11 @@ export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH
 
 # Android Emulator 실행
 emulator -avd android12-play -noaudio -no-boot-anim -gpu swiftshader_indirect -verbose &
-
 sleep 40  # 부팅 대기
 
-# APK 설치 (AAB 없으면 건너뜀)
-/root/bundletool.sh
-/root/install-app.sh
+# APK 설치 (AAB 없으면 skip)
+ /root/bundletool.sh
+ /root/install-app.sh
 
 # 컨테이너 유지
 tail -f /dev/null
