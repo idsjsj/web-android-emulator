2ccb782b02ef7e09b99fbadcb10d14054f78d8be
idsjsj <idsjsj12@gmail.com>
2025-10-19 11:04:01 +0900

    Remove unnecessary blank lines in start-vnc.sh

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-
 Xvfb :0 -screen 0 1280x720x16 &
 export DISPLAY=:0
 fluxbox &
-
 x11vnc -display :0 -nopw -listen localhost -xkb -forever &
 websockify --web=/usr/share/novnc/ 6080 localhost:5900
