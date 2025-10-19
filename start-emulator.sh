00299c382c9e62a8cbe5d4f7ffde7c5a228f99c4
idsjsj <idsjsj12@gmail.com>
2025-10-19 13:49:26 +0900

    Update start-emulator.sh

--- start-emulator.sh
+++ start-emulator.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
+
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH
 
+# VNC 실행
 /root/start-vnc.sh &
 
+# Android Emulator 실행
 emulator -avd android12-play -noaudio -no-boot-anim -gpu swiftshader_indirect -verbose &
-sleep 40
+sleep 40  # 부팅 대기
 
-/root/bundletool.sh
-/root/install-app.sh
+# APK 설치 (AAB 없으면 skip)
+[root]/bundletool.sh
+[root]/install-app.sh
 
+# 컨테이너 유지
 tail -f /dev/null
