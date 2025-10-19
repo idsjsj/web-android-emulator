1cd577e0d8af2a07bf9eaf5693ccbc6b799cb0c7
idsjsj <idsjsj12@gmail.com>
2025-10-19 12:01:50 +0900

    Fix formatting issues in install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:/opt/android-sdk/platform-tools
 adb wait-for-device
 
 if [ -f $APK ]; then
-  adb install -r $APK || echo "[warn] APK 설치 실패"
+    adb install -r $APK || echo "[warn] APK 설치 실패"
 else
-  echo "[info] universal.apk 없음 → 설치 단계 건너뜀"
+    echo "[info] universal.apk 없음 → 설치 단계 건너뜀"
 fi
